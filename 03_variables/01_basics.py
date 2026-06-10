d4b4a52dabddc89edf7e0cdb04ad5889025574dd
AmritSharma26 <amrit2603sharma@gmail.com>
2026-06-10 23:04:25 +0530

    variables, typcasting and user input

---
+++ 03_variables/01_basics.py
@@ -0,0 +1,18 @@
+# In python, variables are used to store data that can be used and manipulated throughout a program. A variable is created the momoent you assign a value to it using the assignment operator (=).
+
+age = 34 # integer
+name = "Harry" #string
+cgpa = 4.55 # float
+
+# Rule of defining a variable in python
+# Variable names must start with a letter (a-z, A-Z) or underscore (_).
+# They can contain letters, numbers and underscores.
+# Variable names are case-sensitive (age and Age are different).
+# Avoid using Python keywords (e.g., if, for, while...) as variables names. 
+
+# 32age = 4 # Invalid because variable cannot start with a number
+age = 32 # valid
+# a$$ge = 45 # Invalid because variables cannot contain special characters other than (_)
+_age = 34
+_nice_45 = 34
+a_b_c_7 = "Sam"
---
+++ 03_variables/02_data_types.py
@@ -0,0 +1,21 @@
+# Python supports several buil-in data types:
+#       Integers (int): whole numbers (e.g., 10, -5).
+#       Floats (float): Decimal numbers (e.g., 3.14, -0.001).
+#       Strings (str): text data enchlosed in qoutes (e.g., "Hello", "Python").
+#       Booleans (bool): REpresents True of False.
+#       Lists: Ordered, mutable collections (e.g., [1, 2, 3]).
+#       Tuples: Ordered, immutable collections (e.g., (1, 2, 3)).
+#       Sets: Unordered collections of unique elements (e,g., {1, 2, 3}).
+#       Dictionaries: Key-value pairs (e.g., {"name": "Alice", "age": 25}).
+
+age = 3
+print(age)
+
+cgpa = 8.2
+print(cgpa)
+
+name = "Amrit"
+print(name)
+
+is_completed = True # can also be Flase
+print(is_completed)
