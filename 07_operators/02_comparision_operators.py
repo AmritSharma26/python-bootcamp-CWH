83572f959cd13bdf84209650dce81f113ea96883
AmritSharma26 <amrit2603sharma@gmail.com>
2026-06-11 09:27:57 +0530

    operators

---
+++ 07_operators/02_comparision_operators.py
@@ -0,0 +1,11 @@
+a = 34
+b = 2
+
+# Comparision Operator
+print(a>4)
+print(a<4)
+print(a<=4)
+print(a>=4)
+print(a==4) # Is a equals to 4?
+print(a==34) # Is a equals to 34?
+print(a!=34) # Is a not equals to 34?
